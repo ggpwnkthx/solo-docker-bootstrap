782458d7c606da697e4386a97a8ed689806da08f
Isaac Jessup <ibjessup@gmail.com>
2024-03-24 13:10:36 -0400

    Update apt.sh

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
+# Initialize a flag to check if any package is not installed
+all_installed=true
+
 # Check if Docker packages are installed
-for pkg in docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; do
-  if dpkg -l | grep -qw $pkg; then
-    echo "Package $pkg is already installed. Exiting..."
-    exit 0
+for pkg in htpassword docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin; do
+  if ! dpkg -l | grep -qw $pkg; then
+    echo "Package $pkg is not installed. Proceeding with installation..."
+    all_installed=false
+    break
   fi
 done
+
+# Exit if all packages are installed
+if $all_installed; then
+  echo "All specified packages are already installed. Exiting..."
+  exit 0
+fi
 
 ## Uninstall conflicting packages
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
@@ -15,7 +25,7 @@ done
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl htpassword
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
