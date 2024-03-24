b27ebfeeab99a03885340638e8908eaf74ac0d01
Isaac Jessup <ibjessup@gmail.com>
2024-03-24 13:24:59 -0400

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,55 +5,50 @@ hash_password() {
   echo "$1" | htpasswd -niB user | cut -d: -f2
 }
 
-# Load the .env file
-if [ -f ".env" ]; then
-    export $(cat .env | sed 's/#.*//g' | xargs)
-else
-    echo "Error: .env file not found."
-fi
+# Helper function to get value from .env file
+get_env_value() {
+  grep "^$1=" .env | cut -d'=' -f2
+}
 
 # Check for PRIMARY_DOMAIN_NAME
-if [ ! $PRIMARY_DOMAIN_NAME ]; then
+primary_domain_name=$(get_env_value PRIMARY_DOMAIN_NAME)
+if [ -z "$primary_domain_name" ]; then
   echo "PRIMARY_DOMAIN_NAME not found in .env file."
   read -p "Enter a primary domain name to use: " primary_domain_name
   echo
-  # Replace or append ADMIN_PASSWORD in .env file
+  # Replace or append PRIMARY_DOMAIN_NAME in .env file
   if grep -q "^PRIMARY_DOMAIN_NAME=" .env; then
-    # Use the 'sed' command to replace the line
     sed -i "/^PRIMARY_DOMAIN_NAME=/c\PRIMARY_DOMAIN_NAME=$primary_domain_name" .env
   else
-    # Append the hashed PRIMARY_DOMAIN_NAME to .env file
     echo "PRIMARY_DOMAIN_NAME=$primary_domain_name" >> .env
   fi
 fi
 
 # Check for ACME_EMAIL
-if [ ! $ACME_EMAIL ]; then
+acme_email=$(get_env_value ACME_EMAIL)
+if [ -z "$acme_email" ]; then
   echo "ACME_EMAIL not found in .env file."
   read -p "Enter an email address to use: " acme_email
   echo
-  # Replace or append ADMIN_PASSWORD in .env file
+  # Replace or append ACME_EMAIL in .env file
   if grep -q "^ACME_EMAIL=" .env; then
-    # Use the 'sed' command to replace the line
     sed -i "/^ACME_EMAIL=/c\ACME_EMAIL=$acme_email" .env
   else
-    # Append the hashed ACME_EMAIL to .env file
     echo "ACME_EMAIL=$acme_email" >> .env
   fi
 fi
 
 # Check for ADMIN_PASSWORD
-if [ ! $ADMIN_PASSWORD ]; then
+admin_password=$(get_env_value ADMIN_PASSWORD)
+if [ -z "$admin_password" ]; then
   echo "ADMIN_PASSWORD not found in .env file."
   read -sp "Enter the admin password to use: " admin_password
   echo
   hashed_password=$(hash_password "$admin_password")
   # Replace or append ADMIN_PASSWORD in .env file
   if grep -q "^ADMIN_PASSWORD=" .env; then
-    # Use the 'sed' command to replace the line
     sed -i "/^ADMIN_PASSWORD=/c\ADMIN_PASSWORD=${hashed_password//\$/\\\$}" .env
   else
-    # Append the hashed ADMIN_PASSWORD to .env file
     echo "ADMIN_PASSWORD=${hashed_password//\$/\\\$}" >> .env
   fi
 fi
